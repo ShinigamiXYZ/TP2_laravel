d8f659d62efc3ba66e88d7b0517be420963651c3
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-03-21 15:01:18 -0400

    adding middlwear -> prevent unauthorized navigation

--- Maisonneuve{e2295344}_V2/resources/views/auth/login.blade.php
+++ Maisonneuve{e2295344}_V2/resources/views/auth/login.blade.php
@@ -11,6 +11,11 @@
         @method('PUT')
 
     <div class="container h-100 ">
+    @if (session('message'))
+    <div class="alert alert-danger">
+        {{ session('message') }}
+    </div>
+@endif
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-12 col-md-9 col-lg-7 col-xl-6">
           <div class="card" style="border-radius: 15px;">
--- Maisonneuve{e2295344}_V2/routes/web.php
+++ Maisonneuve{e2295344}_V2/routes/web.php
@@ -19,11 +19,11 @@ Route::put('/login', [UserController::class, 'auth'])->name('auth.credentials');
 Route::get('/register', [UserController::class, 'register'])->name('auth.register');
 Route::put('/register', [Usercontroller::class, 'createAccount'])->name('aut.createacc');
 
-Route::get('/student', [StudentController::class, 'index'])->name('main.index');
-Route::get('/create', [StudentController::class, 'create'])->name('main.create');
-Route::put('/create', [StudentController::class, 'store'])->name('main.store');
-Route::get('student/{studentId}', [StudentController::class, 'show'])->name('main.show');
-Route::get('edit/{studentId}', [StudentController::class, 'edit'])->name('main.edit');
-Route::put('edit/{studentId}', [StudentController::class, 'update'])->name('main.update');
-Route::delete('edit/{studentId}', [StudentController::class, 'destroy'])->name('main.delete'); 
+Route::get('/student', [StudentController::class, 'index'])->name('main.index')->middleware('auth');
+Route::get('/create', [StudentController::class, 'create'])->name('main.create')->middleware('auth');
+Route::put('/create', [StudentController::class, 'store'])->name('main.store')->middleware('auth');
+Route::get('student/{studentId}', [StudentController::class, 'show'])->name('main.show')->middleware('auth');
+Route::get('edit/{studentId}', [StudentController::class, 'edit'])->name('main.edit')->middleware('auth');
+Route::put('edit/{studentId}', [StudentController::class, 'update'])->name('main.update')->middleware('auth');
+Route::delete('edit/{studentId}', [StudentController::class, 'destroy'])->name('main.delete')->middleware('auth'); 
 
