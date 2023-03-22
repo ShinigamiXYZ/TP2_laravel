bb541d044d185c71c399419a5251ecf5f7a0d685
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-03-22 08:36:41 -0400

    Create Comment Model linked to the new Comments table in DB

--- Maisonneuve{e2295344}_V2/app/Models/Comment.php
+++ Maisonneuve{e2295344}_V2/app/Models/Comment.php
@@ -5,13 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Forum extends Model
+class Comment extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'title',
+        'forum_id',
+        'user_id',
         'content',
-        'user_id'
     ];
 
 
