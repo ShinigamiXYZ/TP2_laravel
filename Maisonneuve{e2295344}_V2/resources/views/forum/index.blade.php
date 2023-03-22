e71ca3ee48d0ad9fdf7fa87aceebf22a2bf6cf52
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-03-22 08:44:36 -0400

    creating first part of comments display and logic

--- Maisonneuve{e2295344}_V2/resources/views/forum/index.blade.php
+++ Maisonneuve{e2295344}_V2/resources/views/forum/index.blade.php
@@ -12,19 +12,39 @@
             @foreach($postlists as $post)
                 <div class="col">
                     <div class="card h-100 shadow-sm border-0 rounded">
-                        <div class="card-body">
+                        <div class="card-body text-center">
                             <h5 class="card-title">{{ $post->title }}</h5>
                             <p class="card-text">{{ $post->content }}</p>
-                            <a href="#" class="btn btn-primary">View Post</a>
+                            
                         </div>
                         <div class="card-footer bg-white border-top-0">
                             <small class="text-muted">{{ $post->created_at->diffForHumans() }}</small> <!-- https://laraveldaily.com/post/carbon-diffforhumans-parameters-and-extra-options -->
                         </div>
                     </div>
-                
+                    <ul class="list-group mt-3">
+    @foreach($post->comments as $comment)
+        <li class="list-group-item">
+            <strong>{{ $comment->user_id }}:</strong> {{ $comment->content }}
+        </li>
+    @endforeach
+</ul>
+
+<form method="POST" class="mt-3">
+    @csrf
+    @method('PUT')
+    <input type="hidden" name="forum_id" value="{{ $post->id }}">
+    <div class="form-group">
+        <label for="content">Add Comment:</label>
+        <textarea name="content" class="form-control" rows="3" required></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary mt-2">Comment</button>
+</form>
                 </div>
             @endforeach
+            
         </div>
+
+   
     </div>
 
 <!-- <///// contenue -->
