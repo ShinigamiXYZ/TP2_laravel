7ca53c68abd3d24aaf27e6ee8de9cf4c86a8a625
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-03-22 08:32:31 -0400

    update and add comments to UserController

--- Maisonneuve{e2295344}_V2/app/Http/Controllers/UserController.php
+++ Maisonneuve{e2295344}_V2/app/Http/Controllers/UserController.php
@@ -13,43 +13,52 @@ class UserController extends Controller
 {
 
     /* Un user se doit d'etre etudiant mais un etudiant n'est pas forcément un utilisateurs */
-    public function login(){
-       
+    /* Un utilisateur doit être un étudiant, mais un étudiant n'est pas forcément un utilisateur */
 
-  if (Auth::check()) {
-    
-    session()->forget('user_id');
-   
-    Auth::logout(); /* Si on retourne a la page de login , ont sort de la session */    
-}
+    public function login(){
+    //fonction de connexion de l'utilisateur
+        if (Auth::check()) {
+            session()->forget('user_id');
+            Auth::logout(); /* Si on retourne a la page de login, on sort de la session */    
+        }
         return view('auth.login');
     }
-    public function register(){
-        $students= Student::all();
 
+    public function register(){
+    //fonction d'enregistrement de l'utilisateur
+        $students= Student::all();
         return view('auth.register', ['students'=>$students]);
     }
 
     public function createAccount(Request $request){
+    //créer un compte utilisateur et l'associer à un étudiant
         $data = $request->only(['fullname', 'username', 'password', 'password_confirmation']);
 
         $validations = [
             'fullname' => 'required',
             'username' => 'required|alpha_num|min:5|max:20',
-            'password' => 'required|min:8|max:25|confirmed|' /* valide que les deux inputs sont identiques */
-            
+            'password' => 'required|min:8|max:25|confirmed|' /* valide que les deux inputs sont identiques */   
         ];
+
+    //validation des données saisies
 
         $validatedData = $request->validate($validations);
 
-        $data['password'] = Hash::make($data['password']); /* Hashing  */
-        $user = User::create($data);/* Creation user */
+    //hashage du mot de passe avant de le stocker dans la base de données
 
+        $data['password'] = Hash::make($data['password']);
+
+    // création de l'utilisateur
+
+        $user = User::create($data);
 
          // Associer user avec l'etudiant
+
         $student = Student::find($data['fullname']);
         $student->user()->associate($user);
         $student->save();
+
+    // redirection vers la page de connexion
 
         return redirect(route('auth.login'))->withSuccess('Account created successfully.'); 
 
@@ -58,8 +67,8 @@ class UserController extends Controller
 
 
     public function auth(Request $request){
+    //authentification de l'utilisateur
         $data = $request->only(['username', 'password']);
-     
 
         if(Auth::attempt($data)){ /* Methode facade Auth */
        
