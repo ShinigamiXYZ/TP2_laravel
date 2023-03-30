b470471620829bdb161bd521ea5efbffb8cab33c
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-03-30 09:58:36 -0400

    add and update comment --FileController

--- Maisonneuve{e2295344}_V2/app/Http/Controllers/FileController.php
+++ Maisonneuve{e2295344}_V2/app/Http/Controllers/FileController.php
@@ -10,10 +10,10 @@ class FileController extends Controller
 {
     public function index()
     {
-        $users = User::all();
-        $files = File::all();
-        $files = File::simplePaginate(2);      
-        return view('files.index', compact('files', 'users'));
+        $users = User::all(); // Récupérer tous les utilisateurs
+        $files = File::all();// Récupérer tous les documents
+        $files = File::simplePaginate(2);// pagination 2 items par page
+        return view('files.index', compact('files', 'users')); // Afficher la vue index avec les fichiers et les utilisateurs
     }
 
     public function create()
@@ -24,14 +24,15 @@ class FileController extends Controller
     public function store(Request $request)
     {
             $request->validate([
-                'file' => 'required|mimes:pdf,zip,doc,docx|max:2048',
+                'file' => 'required|mimes:pdf,zip,doc,docx|max:2048', /* specifications du format et de la tailles accepter */
                 'name' => 'required|max:25|unique:files,name' , /* oublige un nom unique -> plus simple que de crypter */
             ]);
         
-            $uploadedFile = $request->file('file'); 
+            $uploadedFile = $request->file('file'); // Récupérer le fichier téléchargé et le nom de fichier
             $fileTitle = $request->input('name'); /* Creer un titre sans extension pour la BD -> pour comparer unique */
             $fileName = $request->input('name') . '.' . $uploadedFile->getClientOriginalExtension(); /* Creer nom unique mais avec l'extension du document. (simplifie le retrieve) */
-            $filePath = $uploadedFile->storeAs('uploads', $fileName, 'uploads'); 
+            $filePath = $uploadedFile->storeAs('uploads', $fileName, 'uploads');    // Enregistrer le fichier dans le dossier /uploads et stocker son chemin dans la DB
+            /* va se retrouver dans deux dossier du a un lien symblolique entre app/storage et publique via la commande -> php artisan storage:link */
         
             $file = File::create([
                 'name' => $fileTitle,
